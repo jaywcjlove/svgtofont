cad2f421ecc096e50746abbc13d1816f3259214d
jaywcjlove <398188662@qq.com>
2020-08-17 19:11:43 +0800

    test: Add test case.

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,27 @@
-// import { sum } from '../src/utils/sum';
+import fs from 'fs-extra';
+import path from 'path';
 
-describe('sum', () => {
-  it('works', () => {
-    expect(2).toEqual(2);
-  });
+it('Support Less/Sass/Stylus.', async () => {
+  const dir = await fs.readdir(path.join(__dirname, 'example', 'dist'));
+  if (dir && Array.isArray(dir)) {
+    expect([...dir]).toEqual(
+      expect.arrayContaining([
+        expect.stringMatching(/svgtofont\.css$/),
+        expect.stringMatching(/svgtofont\.less$/),
+        expect.stringMatching(/svgtofont\.module\.less$/),
+        expect.stringMatching(/svgtofont\.scss$/),
+        expect.stringMatching(/svgtofont\.styl$/),
+        expect.stringMatching(/svgtofont\.json$/),
+        expect.stringMatching(/svgtofont\.ttf$/),
+        expect.stringMatching(/svgtofont\.eot$/),
+        expect.stringMatching(/svgtofont\.woff$/),
+        expect.stringMatching(/svgtofont\.woff2$/),
+        expect.stringMatching(/svgtofont\.symbol\.svg$/),
+        expect.stringMatching(/symbol.html$/),
+        expect.stringMatching(/font-class.html$/),
+        expect.stringMatching(/index.html$/),
+      ]),
+    );
+    expect(dir.length).toEqual(16);
+  }
 });
