8b6c29c6b64c55a3728e88b9d974262dcad3b751
jaywcjlove <jaywcjlove@users.noreply.github.com>
2024-08-30 08:18:35 +0000

    v5.1.0 feat: use template engine for styles (#240) (#241)
    This reverts commit 668989eeb53562ba0da42136c34249041fcc27d7. 2436312873782f057d629bcc2fb3811829b36c4e

--- example/svgtofont.d.ts
+++ example/svgtofont.d.ts
@@ -3,8 +3,9 @@ export enum Svgtofont {
   Demo = "svgtofont-demo",
   Git = "svgtofont-git",
   Left = "svgtofont-left",
-  Stylelint = "svgtofont-stylelint",
+  Stylelint = "svgtofont-stylelint"
 }
+
 export type SvgtofontClassname = "svgtofont-adobe" | "svgtofont-demo" | "svgtofont-git" | "svgtofont-left" | "svgtofont-stylelint"
 export type SvgtofontIcon = "adobe" | "demo" | "git" | "left" | "stylelint"
 export const SvgtofontPrefix = "svgtofont-"
